7dc285d06dfcc0dc7b390390e8535ebf48c25776
cdunku <rijadcupina6@gmail.com>
2025-11-11 17:00:26 +0100

    Implemented a simple tokenizer

--- src/input.c
+++ src/input.c
@@ -18,8 +18,6 @@ char *getl() {
 
   size_t i = 0;
 
-
-
   while(1) {
     // Since ch is 1 byte long, it will read a maximum of 1 byte.
     byte_read = read(STDIN_FILENO, &ch, sizeof(ch));
@@ -47,7 +45,7 @@ char *getl() {
     // 
     // buffer_size -1 -> buffer size without the NULL character.
     // buffer_size -> includes the \0 character.
-    if(buffer_size - 1 < i) { 
+    if(i >= buffer_size - 1) { 
       buffer_size *= 2;
       char *_buffer = realloc(buffer, buffer_size);
 
@@ -70,11 +68,90 @@ char *getl() {
   return buffer;
 }
 
-char *tokstr(char *str, const char *delim);
+void tokenizer_free(char **argv, char* token, size_t size) {
+  for(size_t i = 0; i < size; i++) {
+    free(argv[i]);
+  } 
+  free(argv);
+  free(token);
+  return;
+}
 
 char **tokenizer(char *str) {
-  char **argv;
+  size_t argv_size = BUFFER_SIZE; 
+  size_t token_size = BUFFER_SIZE;
 
+  char *token = malloc(token_size * sizeof(char));
+  char **argv = malloc(argv_size * sizeof(char *));
 
+  if(token == NULL || argv == NULL) {
+    fprintf(stderr, "Fatal: failed to allocate memory for tokenization\n");
+    exit(EXIT_FAILURE);
+  }
+
+  size_t i = 0, j = 0, k = 0;
+
+  while(str[i] != '\0') {
+    switch (str[i]) { 
+      case ' ':
+      case '\n':
+      case '\t':
+      case '\a':
+        if(k > 0) {
+          token[k] = '\0';
+          char *tokendup = strdup(token);
+          if(tokendup == NULL) {
+            tokenizer_free(argv, token, j);
+
+            fprintf(stderr, "Fatal: failed to allocate memory to prompt\n");
+            exit(EXIT_FAILURE);
+          }
+          argv[j++] = tokendup;
+          k = 0; 
+        }
+        break;
+      default:
+        token[k++] = str[i];
+        break;
+    }
+    if(k >= token_size - 1) {
+      token_size *= 2;
+      char *_token = realloc(token, token_size * sizeof(char));
+      if(_token == NULL) {
+        tokenizer_free(argv, token, j);
+        return NULL;
+      }
+      token = _token;
+    } 
+    if(j >= argv_size - 1) {
+      argv_size *= 2;
+      char **_argv = realloc(argv, argv_size * sizeof(char *));
+      if(_argv == NULL) {
+        tokenizer_free(argv, token, j);
+        return NULL;
+      }
+      argv = _argv;
+    }
+    i++;
+  }
+
+  // If the last character ends with a delimiter that this function hasn't included,
+  // then the last character can be deleted and replaced by '\0'. To prevent this we want 
+  // to check whether if the last token was tokenized or not.
+  if(k > 0) {
+    token[k] = '\0';
+    char *tokendup = strdup(token);
+    if(tokendup == NULL) {
+      tokenizer_free(argv, token, j);
+
+      fprintf(stderr, "Fatal: failed to allocate memory to last character\n");
+      exit(EXIT_FAILURE);
+    }
+    argv[j++] = tokendup;
+  }
+
+  argv[j] = NULL;
+
+  free(token);
   return argv;
 } 
